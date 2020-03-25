16be47fee5207e96f97f7506b231dd52a48223b0
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-25 05:44:01 -0400

    Initial, titlecase still needs work

--- main.js
+++ main.js
@@ -1,10 +1,70 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function triple(num){
+  return num *3
+}
 
+function tripleAll(array){
+  return array.map(triple)
+}
 
+function getNegativeValue(num){
+  if (num > 0){
+    return num * -1
+  } else{
+    return num
+  }
+}
 
+function negativeValues(array){
+  return array.map(getNegativeValue)
+}
 
+function makeQuestion(str){
+  return str + '?'
+}
+
+// function makeEx(str){
+//   return str + '!'
+// }
+
+function questionings(array){
+  return array.map(makeQuestion)
+  //return array.map(makeEx) -_-
+}
+
+const getInitials = name => { let initials = ''; name.split(' ').map( subName => initials = initials + subName[0]); return initials; }; 
+
+function changeToInitials(array){
+  return array.map(getInitials)
+}
+
+function doubleIfEven(num){
+  if (num % 2 == 0){
+    return num * 2
+  }else{
+    return num 
+  }
+}
+
+function doubleEven(array){
+  return array.map(doubleIfEven)
+}
+
+function titleCase(string){
+  return string[0].toUpperCase + string.slice(1).toLowerCase()
+}
+
+function titleCaseNames(string){
+  return string.split(" ").map(letter => titleCase(letter).join(" "))
+}
+
+function doubleEveryOther(num){
+  return num.map(function(value, i){
+    return i % 2 == 0 ? value : value * 2
+  })
+}
 /********************************
  * OUR CODE HERE - DON'T TOUCH! *
  ********************************/
